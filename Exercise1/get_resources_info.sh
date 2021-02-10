1cb04407264e40c6bcba03b2495470ee747342f5
root <root@localhost.localdomain>
2021-02-11 02:29:40 +0530

    first commit

--- Exercise1/get_resources_info.sh
+++ Exercise1/get_resources_info.sh
@@ -4,8 +4,8 @@ cat out.txt |  sed 's/"//g' | sed 's/,//g' |sed 's/\[//g'  |  sed 's/\]//g'| sed
 for line in $(cat result.txt)
 
 do
-echo " Server : $line"
-echo 'ssh geethan@$line'
-#`ssh geethan@$line` ./read_servers_info.sh
+#echo " Server : $line"
+#echo 'ssh geethan@$line'
+`ssh geethan@$line` ./read_servers_info.sh
 done
 
